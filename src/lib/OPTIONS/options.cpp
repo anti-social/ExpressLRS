96b9f73d3c97bc514185c128d8f19d1733e26f4f
Alexander Koval <kovalidis@gmail.com>
2024-10-22 17:12:29 +0300

    740 - 760: 40 channels

--- src/lib/OPTIONS/options.cpp
+++ src/lib/OPTIONS/options.cpp
@@ -56,22 +56,8 @@ __attribute__ ((used)) static firmware_options_t flashedOptions = {
     .domain = 6,
     #elif defined(Regulatory_Domain_US_433_WIDE)
     .domain = 7,
-    #elif defined(Regulatory_Domain_AFU_723_52_747_05_40)
+    #elif defined(Regulatory_Domain_AFU_740_760_40)
     .domain = 8,
-    #elif defined(Regulatory_Domain_AFU_900_1000_100)
-    .domain = 9,
-    #elif defined(Regulatory_Domain_AFU_720_760_80)
-     .domain = 10,
-    #elif defined(Regulatory_Domain_AFU_760_800_80)
-    .domain = 11,
-    #elif defined(Regulatory_Domain_AFU_800_840_80)
-    .domain = 12,
-    #elif defined(Regulatory_Domain_AFU_840_860_40)
-    .domain = 13,
-    #elif defined(Regulatory_Domain_AFU_940_980_80)
-    .domain = 14,
-    #elif defined(Regulatory_Domain_AFU_980_1020_80)
-    .domain = 15,
     #else
     #error No regulatory domain defined, please define one in user_defines.txt
     #endif
